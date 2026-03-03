e657f2b02bcc27d3084151fd9f362cfaf4c2af9d
nurelk112-art <nurelk112@gmail.com>
2026-03-03 15:25:42 +0600

    Homework OOP lesson 1

--- HomeWork/HomeWowk1.py
+++ HomeWork/HomeWowk1.py
@@ -1,1 +1,24 @@
-#smth
+class Car:
+    def __init__(self, name, age, speed):
+        self.name = name
+        self.age = age
+        self.speed = speed
+
+    def description(self):
+        return f"Машина: {self.name}, Возраст: {self.age}, Скорость: {self.speed}"
+
+    def increase_speed(self):
+        self.speed += 10
+        return f"Новая скорость: {self.speed}"
+
+
+# создаём 2 объекта
+car1 = Car("BMW", 3, 120)
+car2 = Car("Audi", 5, 100)
+
+# вызываем методы
+print(car1.description())
+print(car1.increase_speed())
+
+print(car2.description())
+print(car2.increase_speed())
